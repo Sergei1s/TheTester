2b755f05dbaa4c98ba73fc307dbb27293c6c91e8
Sergei1s <sergeikazankov12@gmail.com>
2022-06-02 15:21:33 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {Button, Card} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import TestingCard from './Components/TestingCard';
+import Slider from './Components/Slider';
+import { Tester } from './Pages/Tester';
+
+
+function App() {
+  return (
+    <Tester/>
+  );
+}
+
+export default App;
